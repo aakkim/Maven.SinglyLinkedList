d08bdf8e504f867ac0c0d6c74c4824e0e620a4fd
Angela Kim <aangelakkim@gmail.com>
2023-08-02 11:32:29 -0400

    added toString & add methods and tests

---
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/Node.java
@@ -0,0 +1,36 @@
+package com.zipcodewilmington.singlylinkedlist;
+
+public class Node<T> { //if we take out public and copy this code as an inner class in SinglyLinkedList
+
+    private T data;
+    private Node<T> next;
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(T data) { //create a node, don't care about the next node and set to null
+        this.data = data;
+        this.next = null;
+        // this(data,null); <- is the same thing
+        //we already have a constructor, so we call that constructor
+        // one argument is provided and we assign it, the other we set to null;
+    }
+
+    public T getData() { return data; }
+    // ...
+
+
+    public Boolean hasNext() {
+        return this.next != null;
+    }
+
+    public Node<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node newNode) {
+        this.next = newNode;
+    }
+}
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -5,12 +5,34 @@ import java.util.Comparator;
 /**
  * Created by leon on 1/10/18.
  */
+/*
+
+ */
 public class SinglyLinkedList<T> implements LinkedListIface {
 
+    private Node<T> head = null; //the first node/element in list
     public SinglyLinkedList() {}
 
+    public Boolean isEmpty() { //standard Collection method
+        return head == null; //return true if head is null. if head is null, list is empty
+    }
+
     @java.lang.Override
-    public void add(java.lang.Object data) {
+    public void add(Object data) { //to add an element, find null and then add element
+        Node<T> newNode = new Node(data); //create new node
+        if(this.head == null) {
+            //if head is null then set value of the element to head
+            this.head = newNode;
+        } else {
+            // else set temp node to head
+            Node<T> temp = head;
+            //check if there is a next node. if there is, assign temp to the next node
+            while(temp.hasNext()) {
+                temp = temp.getNext();
+            }
+            // once the .hasNext() hits a null, temp is null so we set it to the newNode
+            temp.setNext(newNode);
+        }
 
     }
 
@@ -47,6 +69,25 @@ public class SinglyLinkedList<T> implements LinkedListIface {
     @java.lang.Override
     public void sort(Comparator comparator) {
 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        //add head
+        sb.append("head -> ");
+        //if head is null, we don't iterate through list. else we loop through and print each data/element
+        if(head != null) {
+            Node<T> temp = head;
+            do { //loop will run once and then check if temp is null or has next
+                //print the data/element
+                sb.append("[" + temp.getData() + "] -> ");
+                temp = temp.getNext();
+            } while(temp != null); // && (temp.hasNext())); //if temp is not null and has a next that's not null
+        }
+        //add NULL
+        sb.append("NULL");
+        return sb.toString();
     }
 }
 
